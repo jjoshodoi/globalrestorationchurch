f9b0080c1d303e4985e9613d9ff314b92d5897cb
Rob <35736957+robertazzopardi@users.noreply.github.com>
2020-07-01 16:39:24 +0100

    rob test commit

--- app/src/main/java/com/example/globalrestorationchurch/MainActivity.java
+++ app/src/main/java/com/example/globalrestorationchurch/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
 
         // epejay epojo
+        // D'banj
     }
 
 }
