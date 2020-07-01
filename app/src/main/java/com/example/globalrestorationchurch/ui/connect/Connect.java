df37c011aadc7f5765e83c84053bcbe3804eec21
joshuaodoi <joshuaodoi@hotmail.co.uk>
2020-07-01 16:56:37 +0100

    Added Notif

---
+++ app/src/main/java/com/example/globalrestorationchurch/ui/connect/Connect.java
@@ -0,0 +1,38 @@
+package com.example.globalrestorationchurch.ui.connect;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.globalrestorationchurch.R;
+
+public class Connect extends Fragment {
+
+    private ConnectViewModel mViewModel;
+
+    public static Connect newInstance() {
+        return new Connect();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.connect_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ConnectViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
