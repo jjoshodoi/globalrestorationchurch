f6a732af32b2415f1c62f4225c1a53991a75679b
joshuaodoi <joshuaodoi@hotmail.co.uk>
2020-07-06 11:29:32 +0100

    Added Notif

---
+++ app/src/main/java/com/example/globalrestorationchurch/ui/chat/ChatFragment.java
@@ -0,0 +1,39 @@
+package com.example.globalrestorationchurch.ui.chat;
+
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.globalrestorationchurch.R;
+
+public class ChatFragment extends Fragment {
+
+    private ChatViewModel mViewModel;
+
+    public static ChatFragment newInstance() {
+        return new ChatFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ChatViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
