38a66f7d915d9b6ec95a755dad76bfb519b6e8c3
rob <robertazzopardi@icloud.com>
2020-07-14 20:59:58 +0100

    Firebase Authentication

--- app/src/main/java/com/example/globalrestorationchurch/MainActivity.java
+++ app/src/main/java/com/example/globalrestorationchurch/MainActivity.java
@@ -68,18 +68,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        // google
-        // https://androidclarified.com/google-signin-android-example/
-        // https://developers.google.com/identity/sign-in/android/sign-in
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestEmail()
-//                .build();
-
-//        googleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        // FIREBASE
-
-
     }
 
     public void createSignInIntent() {
@@ -179,45 +167,6 @@ public class MainActivity extends AppCompatActivity {
         // [END auth_fui_pp_tos]
     }
 
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (resultCode == Activity.RESULT_OK) {
-//            if (requestCode == 101) {
-//                try {
-//                    // The Task returned from this call is always completed, no need to attach
-//                    // a listener.
-//                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//                    GoogleSignInAccount account = task.getResult(ApiException.class);
-//                    onLoggedIn(account);
-//                } catch (ApiException e) {
-//                    // The ApiException status code indicates the detailed failure reason.
-//                    Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-//                }
-//            }
-//        }
-//    }
-//
-//    private void onLoggedIn(GoogleSignInAccount googleSignInAccount) {
-//        Intent intent = new Intent(this, UserDetails.class);
-//        intent.putExtra(UserDetails.GOOGLE_ACCOUNT, googleSignInAccount);
-//
-//        startActivity(intent);
-//        finish();
-//    }
-//
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        GoogleSignInAccount alreadyloggedAccount = GoogleSignIn.getLastSignedInAccount(this);
-//        if (alreadyloggedAccount != null) {
-////            Toast.makeText(this, "Already Logged In", Toast.LENGTH_SHORT).show();
-////            onLoggedIn(alreadyloggedAccount);
-//        } else {
-//            Log.d(TAG, "Not logged in");
-//        }
-//    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -229,6 +178,10 @@ public class MainActivity extends AppCompatActivity {
 //                Intent signInIntent = googleSignInClient.getSignInIntent();
 //                startActivityForResult(signInIntent, 101);
                 createSignInIntent();
+                return true;
+
+            case R.id.action_signout:
+                signOut();
                 return true;
 
             default:
