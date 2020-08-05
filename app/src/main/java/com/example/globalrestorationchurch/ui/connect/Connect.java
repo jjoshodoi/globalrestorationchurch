f519c9930973de56e019400a979e29bb7f1a5ba3
rob <robertazzopardi@icloud.com>
2020-08-05 17:46:20 +0100

    added google maps

--- app/src/main/java/com/example/globalrestorationchurch/ui/connect/Connect.java
+++ app/src/main/java/com/example/globalrestorationchurch/ui/connect/Connect.java
@@ -33,6 +33,8 @@ public class Connect extends Fragment implements OnMapReadyCallback {
         fragmentTransaction.add(R.id.map, mapFragment);
         fragmentTransaction.commit();
 
+        //map fragment
+
         mapFragment.getMapAsync(this);
 
 
